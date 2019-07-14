5ec39f4ca4ad7b5391aa4cfbe29c7d162b9c1e72
Bruce Chastain <bruce.chastain@protonmail.ch>
2019-07-15 00:35:40 +0200

    Add files via upload
    sort of final version. this is simple but works well.

---
+++ photobooth.py
@@ -0,0 +1,31 @@
+import os
+import RPi.GPIO as GPIO
+import time
+import glob
+from PIL import Image
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10, GPIO.OUT)
+GPIO.setwarnings(False)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+os.system("killall gvfsd-gphoto2")
+os.system("gphoto2 --auto-detect")
+time.sleep(2)
+
+def button_callback():
+    os.chdir("count")
+    os.system("feh -F --on-last-slide=quit -f countdown.lst --slideshow-delay .5")
+    os.chdir("..")
+    os.chdir("photos")
+    os.system("gphoto2 --capture-image-and-download")
+    new_pic  = max(glob.iglob('*[Jj][Pp][Gg]'), key=os.path.getctime)
+    os.system("feh -F --on-last-slide=quit --slideshow-delay 7 --start-at " + (new_pic))
+    os.chdir("..")
+
+while True:
+    if GPIO.input(10) == GPIO.HIGH:
+        button_callback()
+    #print("running")
+    #time.sleep(.1)
+
